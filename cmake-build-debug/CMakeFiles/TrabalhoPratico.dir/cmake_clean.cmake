35ae114429bfd8b88fa1ef0eb7c1da90be566ada
Rodrigo Cavadas Mendes <rodrigocm1999@gmail.com>
2020-11-17 16:14:40 +0000

    gae

---
+++ cmake-build-debug/CMakeFiles/TrabalhoPratico.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TrabalhoPratico.dir/main.cpp.obj"
+  "TrabalhoPratico.exe"
+  "TrabalhoPratico.exe.manifest"
+  "TrabalhoPratico.pdb"
+  "libTrabalhoPratico.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TrabalhoPratico.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
